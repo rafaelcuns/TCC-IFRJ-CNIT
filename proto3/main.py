633789e60aa403777ca6710ae66108904975d144
rafaelcuns <rafaplayrj2@gmail.com>
2023-09-07 01:32:17 -0300

    Resolvendo Zero Delay com Raspberry

--- proto3/main.py
+++ proto3/main.py
@@ -5,17 +5,20 @@ import pygame # Conexão com a placa Zero Delay
 laires = serial.Serial("/dev/ttyUSBX", 115200) # ou "COMX", sendo X dependendo da porta
 
 pygame.joystick.init()
-joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
-if len(joysticks) == 0:
-    raise Exception("Placa Zero Delay não conectada!")
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+
+# Para Windows:
+# joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
+# if len(joysticks) == 0:
+#     raise Exception("Placa Zero Delay não conectada!")
 
 pygame.init()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.JOYBUTTONDOWN:
-            match event.dict['button']:
-                case 0:
-                    print('gira')
-                    laires.write(b'gira')
-                case _:
-                    print(event.dict['button'])
+            if event.dict['button'] == 0:
+                print('gira')
+                laires.write(b'gira')
+            else:
+                print(event.dict['button'])
