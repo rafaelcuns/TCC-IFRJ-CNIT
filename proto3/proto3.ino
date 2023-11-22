a0103cf613ef0e3132fc6fedad445cc285f47269
rafaelcuns <rafaplayrj2@gmail.com>
2023-11-21 21:20:20 -0300

    Adicionado suporte para servo contínuo

--- proto3/proto3.ino
+++ proto3/proto3.ino
@@ -1,20 +1,33 @@
 #include <Servo.h>
 
-Servo servo1;
+#define H 180
+#define AH 0
+
+Servo testeServo;
 
 void setup() {
     Serial.begin(115200);
     
-    servo1.attach(22);
-    servo1.write(0);
+    testeServo.attach(22);
+    testeServo.write(0);
 }
 
 void loop() {
     String comando = Serial.readStringUntil('\n');
 
     if (comando == "gira") {
-        servo1.write(180);
+        mover(testeServo, H, 2000);
         delay(2000);
-        servo1.write(0);
+        mover(testeServo, AH, 2000);
     }
 }
+
+void mover(Servo servo, int direcao, int duracao) {
+  servo.write(direcao);
+  delay(duracao);
+  servo.write(90);
+}
+
+// Exemplo:
+// Esse exemplo move o servo na direção horária com velocidade definida por H durante um segundo
+// mover(servo, H, 1000);
